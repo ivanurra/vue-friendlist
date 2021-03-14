70dd9b1d892f8db70481fe2536ddb9994885e085
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-14 09:02:25 +0100

    toggleDetails

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const app = Vue.createApp({
-    data()
+    data() {
+        return {
+            detailsAreVisible: false,
+            friends: [
+                {
+                    id: 'david',
+                    name: 'David Paniagua',
+                    phone: '555 555 555',
+                    email: 'panyagua@localhost.com'
+                },
+                {
+                    id: 'emma',
+                    name: 'Emma Cohen',
+                    phone: '111 111 111',
+                    email: 'emmacohen@localhost.com'
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
 });
 
 app.mount('#app');
