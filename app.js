eb5f237b33432ac32f06c53959a767c2494c89fc
Ivan Urra <ivan.urra.wd@gmail.com>
2021-03-14 11:57:39 +0100

    add component friend-contact

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: 'david',
@@ -16,6 +15,30 @@ const app = Vue.createApp({
                     email: 'emmacohen@localhost.com'
                 }
             ]
+        }
+    }
+});
+
+app.component('friend-contact', {
+    template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'emma',
+                name: 'Emma Cohen',
+                phone: '111 111 111',
+                email: 'emmacohen@localhost.com'
+            }
         }
     },
     methods: {
